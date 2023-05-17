d45b12a48c0ab9dca773e8b75738ea6b133d3e03
Pedra-910 <MarcosViniciusCM@outlook.com>
2023-05-16 21:44:04 -0300

    criaçao do adote.css

--- home/adote/adote.php
+++ home/adote/adote.php
@@ -3,19 +3,20 @@ error_reporting(E_ALL ^ E_WARNING);
 include ('../config/configView.php');
 $result = $db->query("SELECT * FROM dogs ORDER BY id DESC"); 
 ?>
-
-<?php if($result->num_rows > 0){ ?> 
-    <div class="gallery"> 
+<h1>Aumigos de patas</h1>
+<?php if($result->num_rows > 0){ ?> <!--dentro do while tudo se repetar para todos os cachorros-->
+    <link rel="stylesheet" href="adote.css">
         <?php while($row = $result->fetch_assoc()){ ?> 
-            <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image']); ?>" /> 
-            <span>Nome:</span> <?php echo $row['dname']; ?>
+        <div class="gallery" id="galeria"> 
+            <img class="imagem"src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image']); ?>" /> 
+            <span class="nome"><p>Nome:</p></span><p class="nome-cachorro"> <?php echo $row['dname']; ?> </p>
             <br>
-            <span>Sexo:</span> <?php echo $row['sex']; ?>
+            <span><p class="sexo"></p>Sexo:</p></span> <p><?php echo $row['description']; ?></p>
             <br>
-            <span></span> <?php echo $row['description']; ?>
+            <span>Sobre</span> <?php echo $row['sex']; ?>
             <br>
+        </div>
         <?php } ?> 
-    </div> 
 <?php }else{ ?> 
     <p class="status error">Imagens não encontradas...</p> 
 <?php } ?>
