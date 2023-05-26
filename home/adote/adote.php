fc017dae1b6e1c2de57f43b18a2ecd96f9e144fb
Pedra-910 <MarcosViniciusCM@outlook.com>
2023-05-26 14:07:32 -0300

    carrosel do adote

--- home/adote/adote.php
+++ home/adote/adote.php
@@ -3,16 +3,19 @@ include ('../config/configView.php');
 $result = $db->query("SELECT * FROM dogs ORDER BY id DESC"); 
 ?>
 <head><link rel="stylesheet" href="adote.css">
-
+<script src="carroselAdote.js" defer></script>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <h1>Amigos de patas</h1>
-<?php if($result->num_rows > 0){ ?> <!--dentro do while tudo se repetar para todos os cachorros-->
+<?php 
+if($result->num_rows > 0){ ?> <!--dentro do while tudo se repetar para todos os cachorros-->
+<div class="carrousel">
         <?php while($row = $result->fetch_assoc()){ ?> 
-        <div class="gallery" id="galeria"> 
-            <img class="imagem"src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image']); ?>" />
-            <div class="conteudos"> 
-            <div class="dog-nome"><span class="nome"></span> <?php echo $row['dname']; ?> </div>
+            <div class="carrousel-item">
+        <div class="gallery" id="galeria">
+                <img class="imagem" id="img"src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image']); ?>" /> 
+        <div class="conteudos"> 
+        <div class="dog-nome"><span class="nome"></span> <?php echo $row['dname']; ?> </div>
         <br>
             <div class="dog-sexo"><span></span><?php echo $row['sex']; ?></div>
             <br>
@@ -21,7 +24,9 @@ $result = $db->query("SELECT * FROM dogs ORDER BY id DESC");
             <a href="https://api.whatsapp.com/send?phone=553798692510&text= Olá! Estou interessado em adotar o/a <?php echo $row['dname']; ?>.." target="_blank">Clique aqui para entrar em contato via WhatsApp</a>
 </div> 
         </div>
+        </div>
         <?php } ?> 
+        </div>
 <?php }else{ ?> 
     <p class="status error">Imagens não encontradas...</p> 
 <?php } ?>
---
+++ home/adote/carroselAdote.js
@@ -0,0 +1,50 @@
+// Obtém todos os elementos do carrossel
+const carouselItems = document.querySelectorAll('.imagem');
+
+// Define o índice do slide atual
+let currentIndex = 0;
+
+// Função para exibir o slide atual
+function showSlide() {
+  // Oculta todos os slides
+  carouselItems.forEach(item => {
+    item.style.display = 'none';
+  });
+
+  // Exibe o slide atual
+  carouselItems[currentIndex].style.display = 'block';
+}
+
+// Função para exibir o próximo slide
+function nextSlide() {
+  // Incrementa o índice do slide atual
+  currentIndex++;
+
+  // Verifica se o índice ultrapassou o número de slides
+  if (currentIndex >= carouselItems.length) {
+    currentIndex = 0;
+  }
+
+  // Exibe o próximo slide
+  showSlide();
+}
+
+// Função para exibir o slide anterior
+function prevSlide() {
+  // Decrementa o índice do slide atual
+  currentIndex--;
+
+  // Verifica se o índice é menor que zero
+  if (currentIndex < 0) {
+    currentIndex = carouselItems.length - 1;
+  }
+
+  // Exibe o slide anterior
+  showSlide();
+}
+
+// Inicia o carrossel ao carregar a página
+window.addEventListener('load', () => {
+  showSlide();
+  setInterval(nextSlide, 5000); // Intervalo de transição de slide (em milissegundos)
+});
