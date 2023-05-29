560319f9c338a688afbc1fae1b725576d9d97e21
Pedra-910 <MarcosViniciusCM@outlook.com>
2023-05-29 20:58:47 -0300

    index
    style index.php

--- home/adote/viewDog.php
+++ home/adote/viewDog.php
@@ -10,13 +10,19 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
+
 ?>
+<head><link rel="stylesheet" href="viewDog.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 <?php if($result->num_rows > 0){ ?> <!--dentro do while tudo se repetar para todos os cachorros-->
         <?php while($row = $result->fetch_assoc()){ ?>
+          <div class="quadro">
             <img class="imagem"src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image']); ?>" />
             <img class="imagem" onerror="this.style.display='none'" src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image2']); ?>" />
             <img class="imagem" onerror="this.style.display='none'" src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image3']); ?>" />
             <img class="imagem" onerror="this.style.display='none'" src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image4']); ?>" />
+        </div>
 <?php } ?> 
 <?php }else{ ?> 
     <p class="status error">Imagens não encontradas...</p> 
